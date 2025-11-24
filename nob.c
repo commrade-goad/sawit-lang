4b619b8b5d8c773cbe64af79920a19cea37c2664
Fernando Perry <fernandoperry1234@gmail.com>
2025-11-24 14:20:04 +0700

    first

---
+++ nob.c
@@ -0,0 +1,34 @@
+#define NOB_IMPLEMENTATION
+#define NOB_STRIP_PREFIX
+#define PROG_NAME "qlang"
+#include "nob.h"
+
+static Cmd cmd = {0};
+
+static void cflags(Cmd *cmd) {
+    cmd_append(cmd, "-Wall");
+    cmd_append(cmd, "-Wextra");
+    cmd_append(cmd, "-ggdb");
+}
+
+int main(int argc, char **argv) {
+    NOB_GO_REBUILD_URSELF(argc, argv);
+
+    cmd_append(&cmd, "cc");
+    cflags(&cmd);
+    cmd_append(&cmd, "-o", PROG_NAME);
+    cmd_append(&cmd, "main.c");
+
+    cmd_run(&cmd);
+
+    bool run = false;
+    if (argc >= 2) {
+        shift(argv, argc);
+        if (strcmp(argv[0], "-run") == 0) {
+            cmd_append(&cmd, "./"PROG_NAME);
+            cmd_run(&cmd);
+        }
+    }
+
+    return 0;
+}
